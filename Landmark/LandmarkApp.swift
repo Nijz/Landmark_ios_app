518b0aecab4d1e1c397ebdfbd16ba93c07fbc1b3
Nijen Patel <nijenbhut@gmail.com>
2024-11-04 10:03:56 +0530

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Nijen Patel on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
