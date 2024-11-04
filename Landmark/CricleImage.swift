43b228d2d4303746ada4c81c3c3a5d79c9296c2a
Nijen Patel <nijenbhut@gmail.com>
2024-11-04 11:14:56 +0530

    Inital Commit

---
+++ Landmark/CricleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CricleImage.swift
+//  Landmark
+//
+//  Created by Nijen Patel on 04/11/24.
+//
+
+import SwiftUI
+
+struct CricleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CricleImage()
+}
---
+++ Landmark/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmark
+//
+//  Created by Nijen Patel on 04/11/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
