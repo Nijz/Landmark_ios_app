b9fc35df044d1487593e05f9cd9e129c561a427e
Nijen Patel <nijenbhut@gmail.com>
2024-11-05 10:41:14 +0530

    List View commited

---
+++ Landmark/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmark
+//
+//  Created by Nijen Patel on 04/11/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
---
+++ Landmark/Views/LandmarkRowView.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRowView.swift
+//  Landmark
+//
+//  Created by Nijen Patel on 04/11/24.
+//
+
+import SwiftUI
+
+struct LandmarkRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRowView()
+}
